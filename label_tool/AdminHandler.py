f60fcc27dd769455e1aa6e4617228a65ecb23999
Lixing Huang <huanglx1984@hotmail.com>
2012-09-23 17:13:24 -0700

    change admin add data

--- label_tool/AdminHandler.py
+++ label_tool/AdminHandler.py
@@ -15,10 +15,16 @@ class AdminHandler(tornado.web.RequestHandler):
 	def post(self):
 		batch = self.get_argument("batch", None)
 		videos = self.get_argument("videos", None)
+		task_id = self.get_argument("taskid", None)
 		label_type = self.get_argument("type", None)
 		try:
+			if task_id == "" or not task_id:
+				last_one = self.db.query("SELECT max(task_id) as max_id FROM label_video_task_list")
+				last_one = last_one[0]["max_id"]
+				task_id  = int(last_one) + 1
+
 			self.db.execute(
-				"INSERT INTO label_video_task_list (label_type, batch, video_list) VALUES (%s, %s, %s)", label_type, batch, videos
+				"INSERT INTO label_video_task_list (task_id, label_type, batch, video_list) VALUES (%s, %s, %s, %s)", task_id, label_type, batch, videos
 			)
 		except Exception, exception:
 			print exception
